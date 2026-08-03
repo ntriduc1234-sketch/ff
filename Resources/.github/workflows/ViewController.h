524f1bc410035c0da9da236a8fe8737c0432089f
ntriduc1234-sketch <ntriduc1234@gmail.com>
2026-08-03 13:51:46 +0700

    Update ViewController.h

--- Resources/.github/workflows/ViewController.h
+++ Resources/.github/workflows/ViewController.h
@@ -1,4 +1,18 @@
-#import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
+#import "ViewController.h"
+@interface ViewController () { UIButton *btn; }
+@end
+@implementation ViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.blackColor;
+    btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    btn.frame = CGRectMake(50, 100, 200, 50);
+    [btn setTitle:@"KÉO TÂM (OFF)" forState:UIControlStateNormal];
+    [btn setTitleColor:UIColor.greenColor forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(toggle) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+- (void)toggle {
+    [btn setTitle:([btn.titleLabel.text isEqualToString:@"KÉO TÂM (OFF)"]) ? @"KÉO TÂM (ON)" : @"KÉO TÂM (OFF)" forState:UIControlStateNormal];
+}
 @end
